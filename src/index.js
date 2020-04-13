c1eae2d45ad545df24524becb2691c457195643c
JesseGonzalez024 <jessegonzalez024@gmail.com>
2020-04-13 16:19:38 -0400

    react router install + set up

--- src/index.js
+++ src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
+
+import { BrowserRouter as Router} from "react-router-dom";
+
+import {Provider} from 'react-redux';
+import { createStore } from 'redux';
+
+import rootReducer from './reducers/rootReducer'
+import App from './App'; 
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router >
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
