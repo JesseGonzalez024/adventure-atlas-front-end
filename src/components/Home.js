26a161b8ec6836a75e0296eed3dbec454a3698cf
JesseGonzalez024 <jessegonzalez024@gmail.com>
2020-04-13 19:56:12 -0400

    home page css added

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,12 +3,22 @@ import { Link } from 'react-router-dom';
 
 //Presentational Component
 
+const buttonStyle = {
+  backgroundColor: "#2a9fc9",
+  color: "rgb(231, 228, 238)",
+  borderRadius: "10px",
+  fontFamily: "Impact",
+  fontSize: "30px",
+  margin: "15px",
+}
+
+
 const Home = props => (
     <div>
-        <p>Welcome to...</p>
+        <p>Find your local adventure with...</p>
             <h1>Adventure Atlas</h1>
-                <Link to='/adventures'><button>View All Adventures</button></Link>
-                <Link to={'/adventures/new'}><button>Create New Adventure</button></Link>
+                <Link to='/adventures'><button style={buttonStyle} >View All Adventures</button></Link>
+                <Link to={'/adventures/new'}><button style={buttonStyle} >Create New Adventure</button></Link>
     </div>
   )
 
--- src/components/NewAdventure.js
+++ src/components/NewAdventure.js
@@ -3,16 +3,40 @@ import React from 'react'
 class NewAdventure extends React.Component{
 
     state = {
-
+        name:  '',
+        location: '',
+        description: '',
+        photos: [],
+        tags: []
     }
 
+    handleChange = event => {
+        this.setState({
+          [event.target.name]: event.target.value}
+        )  
+      }
 
     render(){
         return (
             <div>
                 <h1>Create a New Adventure!</h1>
                 <form>
-                    
+                    <fieldset>
+                        <label>Name:</label>
+                        <input type="text" onChange={(event) => this.handleChange(event)} name="name" value={this.state.name} />
+                        <br />
+                        <br />
+                        <label>Location:</label>
+                        <input type="text" onChange={(event) => this.handleChange(event)} name="location" value={this.state.location} />
+                        <br />
+                        <br />
+                        <label>Description:</label>
+                        <textarea onChange={(event) => this.handleChange(event)} name="description" value={this.state.description} />
+                        <br />
+                        <br />
+                        <label>Upload Photos:</label>
+                        <input type="text" onChange={(event) => this.handleChange(event)} name="photos" value={this.state.photos} />
+                    </fieldset>
                 </form>
             </div>
         )
