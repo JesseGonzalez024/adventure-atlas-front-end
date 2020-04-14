7330431cee106e6a7e0c30110ddea84bcf3d0ef9
JesseGonzalez024 <jessegonzalez024@gmail.com>
2020-04-13 22:13:53 -0400

    counter logic added to new adventure form state

--- src/components/NewAdventure.js
+++ src/components/NewAdventure.js
@@ -3,6 +3,7 @@ import React from 'react'
 class NewAdventure extends React.Component{
 
     state = {
+        counter: [],
         name:  '',
         location: '',
         description: '',
@@ -13,34 +14,63 @@ class NewAdventure extends React.Component{
     handleChange = event => {
         this.setState({
           [event.target.name]: event.target.value}
-        )  
+        )
+        console.log(this.state)  
       }
 
       handleMorePhotos = event => {
           // If a user selects to add more photos than another input field populates.
+          event.preventDefault()
+          this.setState(previousState => {
+              return {counter: previousState.counter.push("Entry")}
+          })
+          
+          console.log(this.state)
+          
+        //   for (let i = 0; i < this.state.counter; i++) {
+            
+        //   }
+          
+          
       }
 
     render(){
+        // let photoInputs = this.state.counter.length.map((x) => {
+        //     return (      
+        //         <li><input type="text" onChange={(event) => this.handleChange(event)} name="photos" value={this.state.photos} /></li>
+        //       )
+        // })
+        console.log(this.state)
         return (
             <div>
                 <h1>Create a New Adventure!</h1>
+                <h1>{this.state.counter.length > 1 ? "Has none" : this.state.counter}</h1>
                 <form>
                     <fieldset>
-                        <label>Name: </label>
-                        <input type="text" onChange={(event) => this.handleChange(event)} name="name" value={this.state.name} />
-                        <br />
-                        <br />
-                        <label>Location: </label>
-                        <input type="text" onChange={(event) => this.handleChange(event)} name="location" value={this.state.location} />
-                        <br />
-                        <br />
-                        <label>Description: </label>
-                        <textarea onChange={(event) => this.handleChange(event)} name="description" value={this.state.description} />
-                        <br />
-                        <br />
-                        <label>Photo URL: </label>
-                        <input type="text" onChange={(event) => this.handleChange(event)} name="photos" value={this.state.photos} />
-                        <button onClick={(event) => this.handleMorePhotos(event)}> + </button>
+                        <div>
+                            <label>Name: </label>
+                            <input type="text" onChange={(event) => this.handleChange(event)} name="name" value={this.state.name} />
+                            <br />
+                            <br />
+                        </div>
+                        <div>
+                            <label>Location: </label>
+                            <input type="text" onChange={(event) => this.handleChange(event)} name="location" value={this.state.location} />
+                            <br />
+                            <br />
+                        </div>
+                        <div>
+                            <label>Description: </label>
+                            <textarea onChange={(event) => this.handleChange(event)} name="description" value={this.state.description} />
+                            <br />
+                            <br />
+                        </div>
+                        <div>
+                            <label>Photo URL: </label> <button onClick={(event) => this.handleMorePhotos(event)}> + </button>
+                                <ul id="photoUploadUl">
+                                    {/* {photoInputs} */}
+                                </ul>                          
+                        </div>
                     </fieldset>
                 </form>
             </div>
