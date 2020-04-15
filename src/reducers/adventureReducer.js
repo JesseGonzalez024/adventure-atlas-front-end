3480d1b3822a028624286fe2023608d4be08a6da
JesseGonzalez024 <jessegonzalez024@gmail.com>
2020-04-15 02:27:08 -0400

    adventure reducer update && install thunk

--- src/reducers/adventureReducer.js
+++ src/reducers/adventureReducer.js
@@ -1,18 +1,55 @@
 
-const adventureReducer = (state = [], action) => {
+// const adventureReducer = (state = [], action) => {
  
-    // debugger
+//     // debugger
     
-    switch(action.type){
-        case 'ADD_ADVENTURE':
-            return {adventures: action.adventure}
+//     switch(action.type){
+//         case 'ADD_ADVENTURE':
+//             return {adventures: action.adventure}
         
+//         case 'UPVOTE_ADVENTURE':
+//             return {adventure: action.adventure}
+        
+//          default:
+//             return state
+//     }
+// }
+
+// export default adventureReducer
+
+let initialState = {
+    state: [],
+    loading: false
+}
+
+
+const adventureReducer = (state = initialState, action) => {
+
+    debugger
+    switch(action.type) {
+        case 'FETCHING_ADVENTURES':
+            return {
+                ...state,
+                astronauts: [...state.astronauts],
+                loading: true
+            }
+ 
+        case 'ADD_ADVENTURES':
+            return {
+                ...state,
+                adventures: action.adventures,
+                loading: false
+            }
+
         case 'UPVOTE_ADVENTURE':
             return {adventure: action.adventure}
-        
-         default:
-            return state
+ 
+        default:
+            return state;
     }
 }
 
 export default adventureReducer
+
+
+
