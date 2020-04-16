978ba0be90df03dec78cc2312ba64e7dac5b472d
JesseGonzalez024 <jessegonzalez024@gmail.com>
2020-04-15 23:00:25 -0400

    minimal change to newadventure form

--- src/components/NewAdventure.js
+++ src/components/NewAdventure.js
@@ -85,7 +85,7 @@ class NewAdventure extends React.Component{
                                 onChange={(event) => this.handleChange(event)} 
                                 name="photo"
                                 placeholder="Photo URL"  
-                                value={this.state.photo}/>{this.state.photo === '' ? "" : <button onClick={(event) => this.addPhotos(event)} value={this.state.photo}> + </button>}
+                                value={this.state.photo}/>{this.state.photo === '' ? null : <button onClick={(event) => this.addPhotos(event)} value={this.state.photo}> + </button>}
                             <p>{this.state.photoCollection < 1 ? "No photos have been uploaded" : `You have uploaded ${this.state.photoCollection.length} photos! Add another?`}</p>                           
                         </div>
                         <div>
