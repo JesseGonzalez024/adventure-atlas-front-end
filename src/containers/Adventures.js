f29df69acfcc9200014ad2123720da9160272eca
JesseGonzalez024 <jessegonzalez024@gmail.com>
2020-04-24 17:03:32 -0400

    search bar is once more functional

--- src/containers/Adventures.js
+++ src/containers/Adventures.js
@@ -10,26 +10,41 @@ import Tags from './Tags'
 class Adventures extends React.Component{
     
     state = {
-        adventures: []
+        adventures: [],
+        search: false
     }
     
     handleSearchBar = (value) => {
-        let results = this.props.adventures.filter(adv => {
-            return adv.location.includes(value)
-          })
-          console.log(results)
+        if (value) {
+            let results = this.props.adventures.filter(adv => {
+                return adv.location.includes(value)
+              }) 
+              this.setState({adventures: results})
+              this.setState({search: true})
+        }
+        else {
+            this.setState({search: false})
+        }
+    }
 
+    passAdevnturesToList = () => {
+        if (this.props.adventures.length === 0) {
+            return <p>Loading Content...</p>
+        } else if (this.state.search === false){
+            return <List adventures={this.props.adventures}/>
+        } else if (this.state.search === true) {
+            return <List adventures={this.state.adventures}/>
+        }
     }
 
     render() {
-
         return (
             <div>
                 <br />
                 <h1>Adventure Atlas Library</h1>
                 <SearchBar adventures={this.props.adventures} handleSearchBar={this.handleSearchBar}/>
                 <Tags tags={this.props.adventures}/>
-                {this.props.adventures.length > 0 ? <List adventures={this.props.adventures}/> : <p>Loading Content...</p>}
+                {this.passAdevnturesToList()}
             </div>
         )
     };
