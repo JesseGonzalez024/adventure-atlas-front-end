742e8d32c76dfb85e6a1d2ae67431f8f6b412b9c
JesseGonzalez024 <jessegonzalez024@gmail.com>
2020-08-10 21:26:11 -0600

    clean up unnecessary notes

--- src/reducers/rootReducer.js
+++ src/reducers/rootReducer.js
@@ -1,7 +1,5 @@
 import {combineReducers} from 'redux';
 import adventureReducer from './adventureReducer';
-// import photoReducer from './photoReducer'
-// import tagReducer from './tagReducer'
 
 export default combineReducers({
     adventureReducer
